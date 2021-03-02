f37f930d7e452e223056e22d38b3581b99c493eb
dibdas <dibyandu08das@gmail.com>
2021-03-02 20:12:02 +0530

    add files spec/models

---
+++ spec/factories/friendships.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+    factory :friendship do
+      sender
+      receiver
+      status { 'accepted' }
+  
+      trait :pending do
+        status { 'pending' }
+      end
+    end
+  end
