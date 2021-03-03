1eef484bb15810cce9e16e2b1790526d4096bd11
dibdas <dibyandu08das@gmail.com>
2021-03-03 18:29:59 +0530

    added spec files

---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,18 @@
+equire 'rails_helper'
+
+RSpec.describe Friendship, type: :model do
+  subject(:friendship) { build(:friendship) }
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:sender).class_name('User') }
+
+    it { is_expected.to belong_to(:receiver).class_name('User') }
+  end
+
+  describe 'validations' do
+    it {
+      is_expected.to define_enum_for(:status).with_values(accepted: 'accepted',
+                                                          pending: 'pending').backed_by_column_of_type(:string)
+    }
+  end
+end
